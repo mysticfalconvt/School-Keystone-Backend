e45a8284edfc46e8a61c9b3f3d247b2b039e997f
MysticfalconVT <rboskind@gmail.com>
2021-08-18 18:51:11 -0400

    add birthdays mutation added

--- mutations/addBirthdays.ts
+++ mutations/addBirthdays.ts
@@ -1,6 +1,6 @@
 import { KeystoneContext, SessionStore } from '@keystone-next/types';
 import { Session } from '../types';
-import { CalendarUpdateInput } from '../.keystone/schema-types';
+import { BirthdayCreateInput } from '../.keystone/schema-types';
 import { Birthday } from '../schemas/Birthday'
 import { User } from '../schemas/User'
 
@@ -11,33 +11,40 @@ async function addBirthdays(root: any,
     { birthdayData }: { birthdayData: string },
 
     context: KeystoneContext
-): Promise<CalendarUpdateInput> {
+): Promise<BirthdayCreateInput> {
 
     // get JSON array of students and updated schedules
     console.log('Adding Birthdays');
     const birthdayUpdateResults = [];
     const birthdayList = JSON.parse(birthdayData);
+    // console.log(birthdayList);
     //go through each student and update their schedule or create a new student
     await Promise.all(birthdayList.map(async singleBirthday => {
         const student = await context.lists.User.findMany({
-            where: { email: singleBirthday.student }, resolveFields: graphql`
+            where: { email: singleBirthday.email.toLowerCase() }, resolveFields: graphql`
       id
     email
+    birthday{
+        id
+    }
     ` });
+        // console.log(student);
+        if (student.length > 0 && !student[0].birthday) {
 
-        const createdBirthday = await context.lists.Birthday.createOne({
-            data: {
-                date: singleBirthday.date,
-                student: { connect: { id: student[0].id } }
-            },
-            resolveFields: 'id'
-        })
+            const createdBirthday = await context.lists.Birthday.createOne({
+                data: {
+                    date: singleBirthday.date,
+                    student: { connect: { id: student[0].id } }
+                },
+                resolveFields: 'id'
+            })
 
-        birthdayUpdateResults.push(createdBirthday);
+            birthdayUpdateResults.push(createdBirthday);
+        }
     }))
 
-    const name = JSON.stringify(birthdayUpdateResults);
-    return { name }
+    const cakeType = JSON.stringify(birthdayUpdateResults);
+    return { cakeType }
 }
 
 export default addBirthdays;
